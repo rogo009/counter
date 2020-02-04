758f33928eb9e4a011fc180482eb1b220a974a4c
robin <rogo009git@gmail.com>
2020-02-04 07:50:10 -0800

    add reset button add functionality

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 
 const buttonAdd = document.getElementById('add');
 const buttonSub = document.getElementById('minus');
+const buttonReset = document.getElementById('reset');
 let counter = document.getElementById('counter');
-let array = [];
+// let array = []; 
 let count = 0;
 
 // handlers
 
 buttonAdd.addEventListener('click', add);
 buttonSub.addEventListener('click', subtract);
+buttonReset.addEventListener('click', reset);
 
 // function
 
@@ -21,6 +23,15 @@ function add() {
 function subtract() {
     count -=1;
     counter.innerHTML = count;
+}
+
+function reset() {
+    count = 0;
+    counter.innerHTML = count;
+    counter.style.color = 'red';
+    setTimeout(function() {
+        counter.style.color = 'black';
+    }, 1500);
 }
 
 // var button = document.getElementById("clickme"),
