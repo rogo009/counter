47d51a5637f20e23c41c4dcef77e31154ff31914
robin <rogo009git@gmail.com>
2020-02-04 22:19:25 -0800

    update script file

--- script.js
+++ script.js
@@ -49,11 +49,3 @@ function reset() {
     }, 1500);
 }
 
-// var button = document.getElementById("clickme"),
-//   count = 0;
-
-// button.onclick = function() {
-//   count += 1;
-//   button.innerHTML = "Click me: " + count;
-// };
-
